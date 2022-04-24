8ba269342402cf13919799cc666a388dc265a27d
Curtis <curt_king@coolsite.net>
2022-04-24 18:22:54 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PageHome/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import PageHome from '@/components/PageHome'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PageHome     // PageHome: PageHome could be written like this - but we have made use of the ES6 object shortcut 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,32 @@
+<!-- here the template, script + style form the SFC.. Single file component
+// which is a special vue file which allow us to have template, javascript + CSS in one file 
+// visit vuejs.org/v2/style-guide/#Rule-Categories for advise on naming components -->
+<template>
+    <div><p>Hello World, How goes? :)</p></div>
+    <div v-for="thread in threads" :key="thread.id"> <!-- whenever rendering a list of attributes, you should pass a key so vue can keep track of the list, each key should be unique -->
+        <h2>{{thread.title}}</h2>
+        <div v-for="postId in thread.posts" :key="postId">       <!-- remember the importance of the key here -->
+             <p>{{users.find(u => u.id === posts.find(p => p.id === postId).userId).name}}</p>
+             <p>{{posts.find(p => p.id === postId).text}}</p>    <!-- here we search in posts array for post with corresponding ID -->
+        </div> 
+    </div>
+</template>
+
+<script> //here we want to import our user data
+import sourceData from "@/data.json"
+export default {
+    data () {
+        return {
+            threads: sourceData.threads,
+            posts: sourceData.posts,
+            users: sourceData.users
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
